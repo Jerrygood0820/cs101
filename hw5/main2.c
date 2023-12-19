def542dd3842539e73ef72b4a1f16a0dbb35abed
Jerrygood0820 <144075937+Jerrygood0820@users.noreply.github.com>
2023-12-19 21:33:21 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int iterations = 1000000; 
-    double PI = 4.0;
+    double targetPI = 3.14159;
+    double PI = 0.0;
+    int iterations = 0;
 
-    for (int i = 1; i <= iterations; i++) {
+    for (int i = 0; ; i++) {
         double term = 4.0 / (2 * i + 1);
-        
-        
+
+       
         if (i % 2 == 0) {
-            PI -= term;
-        } else {
             PI += term;
+        } else {
+            PI -= term;
+        }
+
+        iterations++;
+
+       
+        if (PI >= targetPI - 0.000005 && PI <= targetPI + 0.000005) {
+            break;
         }
     }
 
+    printf("找到最小的迭代次數：%d\n", iterations);
     printf("使用 %d 次迭代計算的 π 約為：%.5f\n", iterations, PI);
 
     return 0;
